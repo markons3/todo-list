c508021d1e205d99911e7550face4bdcd6cc9a33
Marko Davidovic <marko.davidovic91@gmail.com>
2022-03-22 12:43:54 +0100

    fixed env file returning undefined values

--- server/database.js
+++ server/database.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-require("dotenv").config();
+const dotenv = require('dotenv');
+dotenv.config({path: '../.env'});
 
 module.exports = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_URI);
-
         console.log('Connected to the mongoDB!');
-
+        
     } catch (error) {
         console.log('Could not connect to the mongoDB', error.message)
     }
